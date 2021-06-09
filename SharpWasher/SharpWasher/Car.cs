967b87bdde4547970ec19889c4f07184f717d38e
MoyshaGG <56951957+MoyshaGG@users.noreply.github.com>
2021-06-09 11:43:31 +0300

    Add files via upload

---
+++ SharpWasher/SharpWasher/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpWasher
+{
+    class Car
+    {
+        public string name { get; set; }
+        public bool clean { get; set; }
+
+        public Car(string name)
+        {
+            this.name = name;
+            this.clean = false;
+        }
+        public Car(string name, bool clean)
+        {
+            
+            this.name = name;
+            this.clean = clean;
+        }
+
+        public override string ToString()
+        {
+            if(clean)
+            {
+                return name + " - Чиста";
+            }
+            else
+            {
+                return name + " - Забруднена";
+            }
+        }
+
+    }
+
+}
---
+++ SharpWasher/SharpWasher/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SharpWasher
+{
+    class Program
+    {
+        delegate void WashDelegate(Car car);
+        static void Main(string[] args)
+        {
+
+            Garage garage = new Garage();
+            Car car1 = new Car("Renault");
+            Car car2 = new Car("Skoda");
+            Car car3 = new Car("Porshe", true);
+            Car car4 = new Car("Cadillac");
+            Car car5 = new Car("Bentley");
+
+            garage.Add(car1);
+            garage.Add(car2);
+            garage.Add(car3);
+            garage.Add(car4);
+            garage.Add(car5);
+
+            Console.WriteLine("Список машин та статус очистки.");
+            Console.WriteLine();
+
+            foreach (var index in garage)
+            {
+                Console.WriteLine(index);
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("Чистка...");
+            Console.WriteLine();
+
+            Washer washer = new Washer();
+            WashDelegate washMachine = washer.Wash;
+            foreach (var car in garage)
+            {
+                washMachine(car);
+                Console.WriteLine(car);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
